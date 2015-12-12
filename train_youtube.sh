528d19e8c164a1f138a5d90aef98752087cf0833
Henry <hlin117@gmail.com>
2015-12-11 22:24:53 -0600

    Okay to use 10 threads

--- train_youtube.sh
+++ train_youtube.sh
@@ -6,8 +6,8 @@ g++ -lm -pthread -march=native -Wall -funroll-loops -ffast-math -Wno-unused-resu
 g++ -lm -pthread -march=native -Wall -funroll-loops -ffast-math -Wno-unused-result -g concatenate.cpp -o concatenate
 
 ./reconstruct -train ppi_v10_all7.txt -output net_youtube_dense.txt -depth 2 -k-max 1000
-./line -train net_youtube_dense.txt -output vec_1st_wo_norm.txt -binary 1 -size 128 -order 1 -negative 5 -samples 10000 -threads 4
-./line -train net_youtube_dense.txt -output vec_2nd_wo_norm.txt -binary 1 -size 128 -order 2 -negative 5 -samples 10000 -threads 4
+./line -train net_youtube_dense.txt -output vec_1st_wo_norm.txt -binary 1 -size 128 -order 1 -negative 5 -samples 10000 -threads 10
+./line -train net_youtube_dense.txt -output vec_2nd_wo_norm.txt -binary 1 -size 128 -order 2 -negative 5 -samples 10000 -threads 10
 ./normalize -input vec_1st_wo_norm.txt -output vec_1st.txt -binary 1
 ./normalize -input vec_2nd_wo_norm.txt -output vec_2nd.txt -binary 1
 ./concatenate -input1 vec_1st.txt -input2 vec_2nd.txt -output vec_all.txt -binary 1
